3a80496f8722232a9881d9a565aa3500150b5b43
Sakib Hadziavdic <sakib@sake.ba>
2024-02-12 19:50:18 +0100

    Add files

---
+++ scalaHelloWorld/src/example/Hello.scala
@@ -0,0 +1,8 @@
+package example
+
+object Hello extends App {
+
+  val service = MyService("fssdfs")
+
+}
+
---
+++ scalaHelloWorld/src/example/MyService.scala
@@ -0,0 +1,9 @@
+package example
+
+// 1 initial
+class MyService(str: String)
+
+// 2. still compiles.. :O
+//class MyService(str: String, x: Int)
+
+
